22a198163c450d3ef22316da628544053d5edec3
Andrew Waller <andrewhwaller@gmail.com>
2019-09-19 04:40:34 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -19,6 +19,9 @@ class GithubService
   end
 
   def authenticate!(client_id, client_secret, code)
-
+    response - Faraday.post "https://github.com/login/oauth/access_token",
+      {client_id: client_id, client_secret: client_secret, code: code},
+      {'Accept' => 'application/json'}
+    access_hash = JSON.parse(response.body)
   end
 end
