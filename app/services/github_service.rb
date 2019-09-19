a7e2cb12e886c389662b2065702240c31c4de8d5
Andrew Waller <andrewhwaller@gmail.com>
2019-09-19 04:40:45 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -23,5 +23,6 @@ class GithubService
       {client_id: client_id, client_secret: client_secret, code: code},
       {'Accept' => 'application/json'}
     access_hash = JSON.parse(response.body)
+    @access_token = access_hash["access_token"]
   end
 end
