933bbdeaac11ea4c4c0f8613f4d85c998ae889c2
Andrew Waller <andrewhwaller@gmail.com>
2019-09-19 04:37:07 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,3 +1,24 @@
 class GithubService
 
+  attr_reader :access_token
+
+  def initialize
+
+  end
+
+  def create_repo
+
+  end
+
+  def get_repos
+
+  end
+
+  def get_username
+
+  end
+
+  def authenticate!
+
+  end
 end
