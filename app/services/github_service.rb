d2a610d83546730d6a30a7b7d76a12ac9a765dae
Andrew Waller <andrewhwaller@gmail.com>
2019-09-19 04:44:01 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -12,8 +12,8 @@ class GithubService
 
   def get_repos
     response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
-repos_array = JSON.parse(response.body)
-repos_array.map{|repo| GithubRepo.new(repo) }
+    repos_array = JSON.parse(response.body)
+    repos_array.map{|repo| GithubRepo.new(repo) }
   end
 
   def get_username
